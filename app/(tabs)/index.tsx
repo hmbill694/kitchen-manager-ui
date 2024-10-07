92b9c08f06d24c23d51cff3c94ca81a87182cb7f
Harrison Billings <harry-mac@Harrisons-Laptop.local>
2024-10-06 19:04:31 -0700

    nativewind working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,9 @@
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
-    <View className="bg-red-100">
-      <Text>Tab One</Text>
-      <View className="bg-black" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className="bg-black">
+      <Text className="text-red-200">Tab One! Bitch</Text>
     </View>
   );
 }
